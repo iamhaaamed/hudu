8f10e6ea77cbed757c09e06515ff99821f69a0cc
kamran <kamran.km9090@gmail.com>
2022-05-29 14:14:22 +0430

    add native-base & react-native-svg

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import AppNavigator from '~/navigation/AppNavigator';
+import {NativeBaseProvider} from 'native-base';
 
 const App = () => {
   return (
-    <>
+    <NativeBaseProvider>
       <AppNavigator />
-    </>
+    </NativeBaseProvider>
   );
 };
 
