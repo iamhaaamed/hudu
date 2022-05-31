3587d18ee05c3db8d71cd1af0def48d7a4edac01
kamran <kamran.km9090@gmail.com>
2022-05-31 18:01:53 +0430

    add favorites-screen ui

---
+++ src/components/atoms/EmptyData/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Keyboard, TouchableWithoutFeedback} from 'react-native';
+import {Center, Text} from 'native-base';
+import {fontFamily, scale} from '~/utils/style';
+import {Colors} from '~/styles';
+
+export default function EmptyData({text = 'No Result'}: {text?: string}) {
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Center flex={1}>
+        <Text
+          color={Colors.BLACK_3}
+          fontFamily={fontFamily.regular}
+          fontSize={scale(16)}>
+          {text}
+        </Text>
+      </Center>
+    </TouchableWithoutFeedback>
+  );
+}
