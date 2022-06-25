7b064539ba0b229147d5b4d675eb62369e2cc235
kamran <kamran.km9090@gmail.com>
2022-06-25 12:38:47 +0430

    fix notification item touchable

--- src/components/molecules/NotificationItem/index.tsx
+++ src/components/molecules/NotificationItem/index.tsx
@@ -3,6 +3,7 @@ import {Colors} from '~/styles';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {Text, Badge, HStack, IconButton, Spinner} from 'native-base';
 import {useDeleteNotification, useReadNotification} from '~/hooks/notification';
+import {TouchableOpacity} from 'react-native';
 
 const NotificationItem = ({item}: {item: any}) => {
   const {mutate: mutateReadNotification, isLoading: readNotificationLoading} =
@@ -25,41 +26,48 @@ const NotificationItem = ({item}: {item: any}) => {
     });
   };
 
+  const onPressHandler = () => {};
+
   return (
-    <HStack
-      mb="3"
-      px="2"
-      w="100%"
-      rounded={10}
-      borderWidth="1"
-      overflow="hidden"
-      alignItems="center"
-      borderColor={Colors.GARY_2}>
-      {!item?.isReaded && (
-        <Badge
-          px="1"
-          mr="2"
-          h="3"
-          w="3"
+    <TouchableOpacity
+      disabled={true}
+      activeOpacity={0.7}
+      onPress={onPressHandler}>
+      <HStack
+        mb="3"
+        px="2"
+        w="100%"
+        rounded={10}
+        borderWidth="1"
+        overflow="hidden"
+        alignItems="center"
+        borderColor={Colors.GARY_2}>
+        {!item?.isReaded && (
+          <Badge
+            px="1"
+            mr="2"
+            h="3"
+            w="3"
+            rounded="full"
+            variant="solid"
+            colorScheme="warning"
+          />
+        )}
+        <Text flex={1}>{item?.title}</Text>
+        <IconButton
           rounded="full"
-          variant="solid"
-          colorScheme="warning"
+          onPress={deleteOnPress}
+          colorScheme={Colors.RED_RIPPLE_COLOR}
+          icon={
+            deleteNotificationLoading ? (
+              <Spinner color={Colors.BLACK_3} size={24} />
+            ) : (
+              <Icon name="close" color={Colors.BLACK_3} size={24} />
+            )
+          }
         />
-      )}
-      <Text flex={1}>{item?.title}</Text>
-      <IconButton
-        rounded="full"
-        onPress={deleteOnPress}
-        colorScheme={Colors.RED_RIPPLE_COLOR}
-        icon={
-          deleteNotificationLoading ? (
-            <Spinner color={Colors.BLACK_3} size={24} />
-          ) : (
-            <Icon name="close" color={Colors.BLACK_3} size={24} />
-          )
-        }
-      />
-    </HStack>
+      </HStack>
+    </TouchableOpacity>
   );
 };
 
