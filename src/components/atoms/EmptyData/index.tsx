ff6a41f1e2a5988672dd0e7a4150c4f66c2b6591
kamran <kamran.km9090@gmail.com>
2022-06-29 17:00:31 +0430

    customize emptyData component

--- src/components/atoms/EmptyData/index.tsx
+++ src/components/atoms/EmptyData/index.tsx
@@ -4,10 +4,16 @@ import {Center, Text} from 'native-base';
 import {fontFamily, scale} from '~/utils/style';
 import {Colors} from '~/styles';
 
-export default function EmptyData({text = 'No result'}: {text?: string}) {
+export default function EmptyData({
+  text = 'No result',
+  flex = 1,
+}: {
+  text?: string;
+  flex?: number;
+}) {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <Center flex={1}>
+      <Center flex={flex}>
         <Text
           color={Colors.EMPTY_DATA}
           fontFamily={fontFamily.regular}
